c6ce8d79ae256a37bf61b6e01a82b792bec7c331
yhk17 <yhk17@foxmail.com>
2023-02-11 22:44:20 +0800

    :poop: 全是bug

--- init.lua
+++ init.lua
@@ -14,21 +14,77 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
-	{"morhetz/gruvbox", lazy = false, priority = 1000, },
+	opts = {colorscheme = "gruvbox",},
+	{"ellisonleao/gruvbox.nvim", lazy = false, priority = 1000, },
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   "folke/neodev.nvim",
   "vim-airline/vim-airline",
   "vim-airline/vim-airline-themes",
-{
+  -- neotree文件树
+  {
   "nvim-neo-tree/neo-tree.nvim",
-    keys = {
-      { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+  cmd = "Neotree",
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
+      end,
+      desc = "Explorer NeoTree (root dir)",
     },
-    config = function()
-      require("neo-tree").setup()
-    end,
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+  },
+  deactivate = function()
+    vim.cmd([[Neotree close]])
+  end,
+  init = function()
+    vim.g.neo_tree_remove_legacy_commands = 1
+    if vim.fn.argc() == 1 then
+      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == "directory" then
+        require("neo-tree")
+      end
+    end
+  end,
+  opts = {
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = true,
+    },
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+      },
+    },
+  },
 },
+{
+    "nvim-neorg/neorg",
+    -- lazy-load on filetype  {
+    "cshuaimin/ssr.nvim",
+    -- init is always executed during startup, but doesn't load the plugin yet.
+    init = function()
+      vim.keymap.set({ "n", "x" }, "<leader>cR", function()
+        -- this require will automatically load the plugin
+        require("ssr").open()
+      end, { desc = "Structural Replace" })
+    end,
+  },
+  -- 用于符号对齐，格式化代码使用
+  "godlygeek/tabular",
+  -- coc
+  {"neoclide/coc.nvim", branch = "release"},
+  --高亮光标下边的单词
+  "RRethy/vim-illuminate",
 -- UI美化
 { "stevearc/dressing.nvim", event = "VeryLazy" },
 })
