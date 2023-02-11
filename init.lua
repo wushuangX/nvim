98e60c8d08851a3e8b902bbc7298e0ed4a591913
yhk17 <yhk17@foxmail.com>
2023-02-11 22:02:17 +0800

    :sparkles: 初步修改

--- init.lua
+++ init.lua
@@ -14,7 +14,22 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
+	{"morhetz/gruvbox", lazy = false, priority = 1000, },
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   "folke/neodev.nvim",
+  "vim-airline/vim-airline",
+  "vim-airline/vim-airline-themes",
+{
+  "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+    },
+    config = function()
+      require("neo-tree").setup()
+    end,
+},
+-- UI美化
+{ "stevearc/dressing.nvim", event = "VeryLazy" },
 })
+
